adba0bd82e7be0d8cace3581d525cfbd01ab9f2d
sunilskut <sunilskut@gmail.com>
2025-06-10 20:13:17 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWmCyJepagKmTy5kfBCOdaGKmcOymj4zg",
+  authDomain: "sunivox.firebaseapp.com",
+  projectId: "sunivox",
+  storageBucket: "sunivox.firebasestorage.app",
+  messagingSenderId: "866107015527",
+  appId: "1:866107015527:web:2c0733694e797dd7586fc2",
+  measurementId: "G-06NVXJVKV2"
+};
